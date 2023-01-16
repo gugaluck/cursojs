7c6a73c114b928fec0d43b177f6c228f478a62da
gugaluck <53919975+gugaluck@users.noreply.github.com>
2023-01-16 09:49:26 -0300

    Create script.js

---
+++ 11/script.js
@@ -0,0 +1,17 @@
+var idade = 14;
+
+if(idade > 18){
+    console.log("Pode fazer");
+}
+else {
+    console.log("não pode fazer")
+}
+
+if(true) {
+    console.log("teste if");
+}
+
+if(false) {
+    console.log("teste if");
+}
+
